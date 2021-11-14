33362998d31c0991a9175f880c7fd2166903e449
davidwobido <davidwobido@googlemail.com>
2021-11-14 11:53:50 +0100

    :lipstick: Change element colors

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 function setup() {
   createCanvas(innerWidth, innerHeight);
-  createGraphics(40, 40);
 }
 
 function draw() {
   if (mouseIsPressed === true) {
+    fill(255, 233, 0);
+    stroke(110);
     ellipse(mouseX, mouseY, 10, 10);
-    line(mouseX, mouseY, 10, 10);
+    line(mouseX, mouseY, innerWidth / 2, innerHeight / 2);
   }
 }
