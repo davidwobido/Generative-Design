964b841ed4bb6ad14c28c9ab68652302b9b07fc1
davidwobido <davidwobido@googlemail.com>
2021-11-14 12:30:04 +0100

    :sparkles: Add features when mouse and keys are clicked

--- sketch.js
+++ sketch.js
@@ -1,12 +1,32 @@
+let counter = 0;
+
 function setup() {
   createCanvas(innerWidth, innerHeight);
 }
 
 function draw() {
-  if (mouseIsPressed === true) {
+  if (mouseIsPressed && counter === 0) {
+    counter++;
+    fill(255, 0, 0);
+    ellipse(mouseX, mouseY, 80, 80);
+    console.log(counter);
+  } else if (mouseIsPressed && counter === 1) {
+    counter++;
+    ellipse(mouseX, mouseY, 80, 80);
+    fill(0, 255, 0);
+    console.log(counter);
+  } else if (mouseIsPressed && counter === 2) {
+    ellipse(mouseX, mouseY, 80, 80);
+    fill(0, 0, 255);
+    console.log(counter);
+  } else {
     fill(255, 233, 0);
-    stroke(110);
+    stroke(240);
     ellipse(mouseX, mouseY, 10, 10);
     line(mouseX, mouseY, innerWidth / 2, innerHeight / 2);
   }
+}
+function keyTyped() {
+  line(mouseX, mouseY, 10, 40);
+  counter === 1;
 }
